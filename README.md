458f48cef24d1d45c5a2f890e7d7a5890e0bd7e2
andreashgk <andreas@hgk.me>
2026-01-11 13:10:43 +0100

    refactor: use only the fhs package variant
    The application itself may also need some libraries, so using an FHS
    environment seems to make more sense.

--- README.md
+++ README.md
@@ -3,11 +3,8 @@
 Small derivation to run the hytale launcher on nixos without flatpaks.
 It can be run with the following command:
 ```
-nix run github:andreashgk/hytale-nix#hytale-launcher-fhs
-```
-
-There is also a non-fhs package, but it currently breaks whenever there is an update available:
-```
 nix run github:andreashgk/hytale-nix#hytale-launcher
 ```
-I'm currently looking into how to disable auto updating.
+
+This package currently breaks whenever there is an update available.
+When I find a way to disable auto-updating for the launcher I will include this here.
--- flake.nix
+++ flake.nix
@@ -8,43 +8,27 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-      version = "2026.01.10-5fdaa5c";
+      version = "2026.01.10-ff8feba";
       hytale-launcher-bin = pkgs.fetchzip {
         url = "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-${version}.zip";
-        sha256 = "sha256-cnFgrn4YW4SuAvmlPfuW0lGXziFat61W+tj1LoQbsOQ=";
+        sha256 = "sha256-EDdTnCWjZgIK6EKyW7H58rL/cbKLdULoefSlxhx4TBY=";
       };
-
-      libraries =
-        p: with p; [
-          libsoup_3
-          gdk-pixbuf
-          glib
-          gtk3
-          webkitgtk_4_1
-        ];
     in
     {
-      packages.x86_64-linux.hytale-launcher = pkgs.stdenv.mkDerivation {
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hytale-launcher;
+
+      packages.x86_64-linux.hytale-launcher = pkgs.buildFHSEnv {
         pname = "hytale-launcher";
         inherit version;
 
-        src = hytale-launcher-bin;
-
-        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-
-        buildInputs = libraries pkgs;
-
-        installPhase = ''
-          mkdir -p $out/bin
-          cp hytale-launcher $out/bin/
-        '';
-      };
-
-      packages.x86_64-linux.hytale-launcher-fhs = pkgs.buildFHSEnv {
-        pname = "hytale-launcher";
-        inherit version;
-
-        targetPkgs = p: libraries p;
+        targetPkgs =
+          p: with p; [
+            libsoup_3
+            gdk-pixbuf
+            glib
+            gtk3
+            webkitgtk_4_1
+          ];
         runScript = "${hytale-launcher-bin}/hytale-launcher";
       };
     };
